1f27bdc11b6ef08589bff65bc9e3fbdb2c898140
Shadowdx24 <nanavatiarpen45@gmail.com>
2024-07-19 18:26:36 +0530

    Added Functionality for Score, High Score System and BG

--- Assets/Scripts/BallControler.cs
+++ Assets/Scripts/BallControler.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,12 +11,63 @@ public class BallControler : MonoBehaviour
     [SerializeField] private GameObject levelWinObj;
     [SerializeField] private MeshRenderer ballRenderer;
     [SerializeField] private Material[] ballMaterials;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI highScoreText;
+    [SerializeField] private TextMeshProUGUI scoreText1;
+    [SerializeField] private TextMeshProUGUI highScoreText1;   
+    [SerializeField] private TextMeshProUGUI scoreText2;
+    [SerializeField] private TextMeshProUGUI highScoreText2;
     private int currball;
+    private int score;
+    private int highScore;
 
     void Start()
     {
+        if (SceneManager.GetActiveScene().name == "Level 1")
+        {
+            score = 0;
+            scoreText.text = "" + score;
+        }
+        else if (SceneManager.GetActiveScene().name == "Level 2")
+        {
+            score = PlayerPrefs.GetInt("Score");
+            scoreText.text = "" + score;
+        }
+        else if (SceneManager.GetActiveScene().name == "Level 3")
+        {
+            score = PlayerPrefs.GetInt("Score");
+            scoreText.text = "" + score;
+        }
+        else
+        {
+            score = 0;
+        }
+        highScore = PlayerPrefs.GetInt("Highscore");
+        highScoreText.text = "" + highScore;
+        //addScore = 0;
         currball = PlayerPrefs.GetInt("MainBall");
         ballRenderer.material = ballMaterials[currball];
+    }
+
+    public void AddScore(int s)
+    {
+        score += s;
+        scoreText.text = "" + score;
+        Debug.Log(score);
+        PlayerPrefs.SetInt("Score", score);
+        CheckHighScore();
+    }
+
+    private void CheckHighScore()
+    {
+        if (highScore < score)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("Highscore", highScore);
+            highScoreText.text = "" + highScore;
+            Debug.Log(highScore);
+
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -43,12 +95,16 @@ public class BallControler : MonoBehaviour
     {
         gameOverObj.SetActive(true);
         Time.timeScale = 0f;
+        scoreText1.text = "Score : " + score;
+        highScoreText1.text = "High Score : " + highScore;
     }
 
     private void LevelWin()
     {
         levelWinObj.SetActive(true);
         Time.timeScale = 0f;
+        scoreText2.text = "Score : " + score;
+        highScoreText2.text = "High Score : " + highScore;
     }
 
     public void GameRestart()
@@ -56,6 +112,7 @@ public class BallControler : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         Time.timeScale = 1f;
         gameOverObj.SetActive(false);
+        score = 0;
     }
 
     public void NextLevel()
---
+++ Assets/Scripts/Ring.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    private Transform ball;
+    [SerializeField] private BallControler Ball;
+
+
+    private void Awake()
+    {
+        ball = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y > ball.position.y + .1f)
+        {
+            
+            Ball.AddScore(1);
+           
+            this.enabled = false;
+        }
+    }
+
+   
+}
